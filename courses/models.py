9037d236d790c7770e05c668e89b9618572b5465
dom-inic <dominicnyambane22@gmail.com>
2023-08-25 22:33:31 +0300

    course, topic models

--- courses/models.py
+++ courses/models.py
@@ -12,3 +12,25 @@ class Subject(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name="courses_created",on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name="courses", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Topic(models.Model):
+    course = models.ForeignKey(Course, related_name='topics', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
