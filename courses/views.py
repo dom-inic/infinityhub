85b3f64b44f4c58426e3ba8ff0bc40a417262e5a
dom-inic <dominicnyambane22@gmail.com>
2023-09-20 22:53:51 +0300

    contentcreateupdateview

--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,14 @@
 
+from pickle import NONE
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.base import TemplateResponseMixin, View
-from . models import Course
+from django.forms.models import modelform_factory
+from django.apps import apps
+from . models import Course, Topic,Content
 from . forms import TopicFormSet
 
 class OwnerMixin(object):
@@ -64,7 +67,48 @@ class CourseTopicUpdateView(TemplateResponseMixin, View):
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course, 'formset': formset})
         
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    topic = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
 
+    def get_model(self, model_name):
+        if model_name in ['text', 'image', 'video', 'file']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
     
-
-
+    def get_form(self,model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner',
+                                                'order',
+                                                'created',
+                                                'updated'
+                                                ])
+        return Form(*args, **kwargs)
+    
+    def dispatch(self,request,topic_id, model_name, id=None):
+        self.topic = get_object_or_404(Topic, id=topic_id,course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model, id=id,owner=request.user) # type: ignore
+        return super().dispatch(request,topic_id,model_name,id)
+    
+    def get(self,request, topic_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+    
+    def post(self, request, topic_id, model_name, id=None):
+        form = self.get_form(self.model, 
+                            instance = self.obj, 
+                            data = request.POST,
+                            files = request.FILES
+                            )
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user 
+            obj.save()
+            if not id:
+                # new content 
+                Content.objects.create(topic=self.topic, item=obj)
+            return redirect('topic_content_list', self.topic.id) # type: ignore
+        return self.render_to_response({'form': form, 'object': self.obj})
