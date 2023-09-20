45c30ba68524c4d2f7c5146495e74723f42fb61e
dom-inic <dominicnyambane22@gmail.com>
2023-09-20 16:16:55 +0300

    managing course topics

---
+++ courses/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+from django.forms.models import inlineformset_factory
+from . models import Course, Topic
+
+TopicFormSet = inlineformset_factory(
+    Course,
+    Topic,
+    fields=['title', 'description'],
+    extra=2,
+    can_delete=True
+    )
