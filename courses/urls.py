5129b8fbd850e13ea620d80939fb0a30dd15f63a
dom-inic <dominicnyambane22@gmail.com>
2023-09-22 19:11:13 +0300

    content create and update

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     path('create/', views.CourseCreateView.as_view(), name="course_create"), 
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'), 
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('<pk>/topic/', views.CourseTopicUpdateView.as_view(), name='course_topic_update')
+    path('<pk>/topic/', views.CourseTopicUpdateView.as_view(), name='course_topic_update'),
+    path(
+        'topic/<int:topic_id>/content/<model_name>/create/',
+        views.ContentCreateUpdateView.as_view(),
+        name="topic_content_create"
+        ),
+    path(
+        'topic/<int:topic_id>/content/model_name/<id>/',
+        views.ContentCreateUpdateView.as_view(),
+        name='topic_content_update'
+        ),
 ]
