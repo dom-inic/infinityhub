8fe46ae85eaf9f82815744f311ee531ac918dfda
dom-inic <dominicnyambane22@gmail.com>
2023-10-04 13:02:01 +0300

    access contents for students

--- infinityhub/settings.py
+++ infinityhub/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
--- students/views.py
+++ students/views.py
@@ -1,11 +1,17 @@
+from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . forms import CourseEnrollForm
+from courses.models import Course
 
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
@@ -30,3 +36,32 @@ class StudentEnrollCourseView(LoginRequiredMixin,FormView):
     
     def get_success_url(self) -> str:
         return reverse_lazy('student_course_detail', args=[self.course.id])
+    
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs =  super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+    
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'topic_id' in self.kwargs:
+            # get current topic
+            context['topic'] = course.topics.get(id=self.kwargs['topic_id']) # type: ignore
+        else:
+            # get first topic 
+            context['topic'] = course.topics.all()[0] # type: ignore
+        return context
+    
