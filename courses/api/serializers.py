c9be9fa2227e7274c33b9a2434661f5bd5128b27
dom-inic <dominicnyambane22@gmail.com>
2023-10-09 11:16:04 +0300

    nested serializer for topic and course

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from ..models import Subject
+from ..models import Subject, Course, Topic
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['order', 'title', 'description']
+
+class CourseSerializer(serializers.ModelSerializer):
+    topics = TopicSerializer(many=True, read_only=True)
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'topics']
